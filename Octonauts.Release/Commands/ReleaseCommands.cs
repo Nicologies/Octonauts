b5a68dc1a244d721348bbeeecc4c0c6ea1ceae7b
Eben Zhang <zhangyingneng@gmail.com>
2018-06-10 23:15:49 +0800

    Just move code to more proper class

--- Octonauts.Release/Commands/ReleaseCommands.cs
+++ Octonauts.Release/Commands/ReleaseCommands.cs
@@ -1,8 +1,11 @@
 using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Threading.Tasks;
 using coreArgs.Attributes;
 using Octonauts.Core;
+using Octonauts.Release.ReleaseCmdHandlers;
 
 namespace Octonauts.Release.Commands
 {
@@ -21,10 +24,44 @@ namespace Octonauts.Release.Commands
         [Option("command", "command to execute", required: true)]
         public string Command { get; set; }
 
-        public static string GetHelpText()
+        private static string GetHelpText()
         {
             var cmds = Enum.GetValues(typeof(Commands)).Cast<Commands>().Select(x => x.GetDescription());
             return "Supported commands are: " + string.Join(",", cmds);
         }
+
+        public static async Task DispatchCommand(string[] args)
+        {
+            var options = CommandArgsParaser.Parse<ReleaseCommands>(args);
+            if (Dispatcher.TryGetValue(options.Command.ToLowerInvariant(), out var handler))
+            {
+                await handler.Handle(args);
+            }
+            else
+            {
+                Console.WriteLine(GetHelpText());
+                Environment.Exit(-1);
+            }
+        }
+
+        private static readonly Dictionary<string, ICommandHandler> Dispatcher = new Dictionary<string, ICommandHandler>
+        {
+            {
+                Commands.CreateReleaseCmd.GetDescription(), new CreateReleaseCmdHandler()
+            },
+            {
+                Commands.PromoteToChannelCmd.GetDescription(), new PromoteToChannelCmdHandler()
+            },
+            {
+                Commands.DeleteReleaseCmd.GetDescription(), new DeleteReleaseCmdHandler()
+            },
+            {
+                Commands.UpdateReleaseVariablesCmd.GetDescription(),
+                new UpdateReleaseVariablesCmdHandler()
+            },
+            {
+                Commands.HelpCmd.GetDescription(), new HelpCmdHandler()
+            },
+        };
     }
 }
--- Octonauts.Release/Program.cs
+++ Octonauts.Release/Program.cs
@@ -1,46 +1,13 @@
-using System;
-using System.Collections.Generic;
 using System.Threading.Tasks;
-using Octonauts.Core;
 using Octonauts.Release.Commands;
-using Octonauts.Release.ReleaseCmdHandlers;
 
 namespace Octonauts.Release
 {
     internal class Program
     {
-        private static readonly Dictionary<string, ICommandHandler> Dispatcher = new Dictionary<string, ICommandHandler>
-        {
-            {
-                ReleaseCommands.Commands.CreateReleaseCmd.GetDescription(), new CreateReleaseCmdHandler()
-            },
-            {
-                ReleaseCommands.Commands.PromoteToChannelCmd.GetDescription(), new PromoteToChannelCmdHandler()
-            },
-            {
-                ReleaseCommands.Commands.DeleteReleaseCmd.GetDescription(), new DeleteReleaseCmdHandler()
-            },
-            {
-                ReleaseCommands.Commands.UpdateReleaseVariablesCmd.GetDescription(),
-                new UpdateReleaseVariablesCmdHandler()
-            },
-            {
-                ReleaseCommands.Commands.HelpCmd.GetDescription(), new HelpCmdHandler()
-            },
-        };
-
         public static async Task Main(string[] args)
         {
-            var options = CommandArgsParaser.Parse<ReleaseCommands>(args);
-            if (Dispatcher.TryGetValue(options.Command.ToLowerInvariant(), out var handler))
-            {
-                await handler.Handle(args);
-            }
-            else
-            {
-                Console.WriteLine(ReleaseCommands.GetHelpText());
-                Environment.Exit(-1);
-            }
+            await ReleaseCommands.DispatchCommand(args);
         }
     }
 }
