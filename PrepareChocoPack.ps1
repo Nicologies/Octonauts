b0a97201d68014bd35cca074d2ac8dd7bb2db26b
Eben Zhang <zhangyingneng@gmail.com>
2020-10-18 10:38:51 +0800

    Feat/Fix: change package name as dot is not allowed for choco package

--- PrepareChocoPack.ps1
+++ PrepareChocoPack.ps1
@@ -1,16 +1,17 @@
 Copy-Item ./LICENSE.txt ./Octonauts.Cli/LICENSE.txt
 $pkg = (Get-Item ./nuget_packages/*.nupkg).FullName
+Remove-Item -Recurse -Force ./temp -ErrorAction SilentlyContinue
 New-Item -Type Directory ./temp -ErrorAction SilentlyContinue
-Move-Item $pkg ./temp/temp.zip
+Copy-Item $pkg ./temp/temp.zip
 Expand-Archive ./temp/temp.zip -DestinationPath ./temp
-$content = Get-Content ./temp/Octonauts.Cli.nuspec -Raw
+$content = Get-Content ./temp/OctonautsCli.nuspec -Raw
 $replaceWith = @"
 </metadata>
-<files>
-        <file src=".\publish\*.*" target="tools"/>
-        <file src="..\LICENSE.txt"/>
-        <file src=".\VERIFICATION.txt"/>
-    </files>
+  <files>
+    <file src=".\publish\*.*" target="tools"/>
+    <file src="..\LICENSE.txt"/>
+    <file src=".\VERIFICATION.txt"/>
+  </files>
 "@
 $content = $content.Replace("</metadata>", $replaceWith)
-Set-Content ./Octonauts.Cli/Octonauts.Cli.nuspec $content
+Set-Content ./Octonauts.Cli/OctonautsCli.nuspec $content
